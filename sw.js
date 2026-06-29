dd2e3c070b7f101ff87d2b37491891ae720886f4
Anuwat <v.anuwatr@gmail.com>
2026-06-29 20:24:18 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,16 @@
-const C='genos-v1';
+const C='genos-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()).catch(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
   const req=e.request;
   if(req.method!=='GET')return;
-  e.respondWith(
-    caches.match(req).then(hit=> hit || fetch(req).then(res=>{
-      try{const copy=res.clone();caches.open(C).then(c=>c.put(req,copy));}catch(_){}
-      return res;
-    }).catch(()=> caches.match('./index.html')))
-  );
+  if(req.mode==='navigate'){
+    e.respondWith(fetch(req).then(res=>{try{const c=res.clone();caches.open(C).then(x=>x.put('./index.html',c));}catch(_){}return res;}).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  e.respondWith(caches.match(req).then(hit=> hit || fetch(req).then(res=>{
+    try{const c=res.clone();caches.open(C).then(x=>x.put(req,c));}catch(_){}
+    return res;
+  }).catch(()=>undefined)));
 });
